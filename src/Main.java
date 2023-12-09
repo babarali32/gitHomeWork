8adfb6526068e71c7f2039c13a9d63c07ac011e8
asghar <babaraliengineer32@gmail.com>
2023-12-09 18:30:53 +0500

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println("you are modifier");
+        System.out.println(" i am at working directory");
     }
 }
