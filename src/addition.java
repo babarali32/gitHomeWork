e6144f701df8f0a39dd43f176f55ef8e78f0a0c5
asghar <babaraliengineer32@gmail.com>
2023-12-10 16:14:31 +0500

    downcasting vs upcasting

---
+++ src/addition.java
@@ -0,0 +1,13 @@
+public class addition {
+    public static void main(String[] args) {
+        double a=90;
+        int b=(int) a;
+        //downcasting
+        System.out.println(b);
+
+        // upcasting
+        int x=4;
+        double y=x;
+        System.out.println(y);
+    }
+}
