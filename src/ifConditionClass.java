8602d9b11941d1afb77fc1b6a89d1a0afbfa118e
asghar <babaraliengineer32@gmail.com>
2023-12-10 14:59:35 +0500

    if condition code

---
+++ src/ifConditionClass.java
@@ -0,0 +1,10 @@
+public class ifConditionClass {
+    public static void main(String[] args) {
+        int age=13;
+        if(age>18){
+            System.out.println(" your are eligible");
+        }else {
+            System.out.println(" play with pogu game");
+        }
+    }
+}
